27ce7a1add7996aa34a0af561f64096a5db6b338
jek10 <jekalviar10@gmail.com>
2016-05-27 17:16:07 +0800

    Add markdown

--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "/var/www/static/media")
 
 
 
